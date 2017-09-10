087378c12f28da84920220b708a6e382fd96d702
Liam Lei <775138281@qq.com>
2017-09-10 16:11:46 -0400

    updates

--- main.cpp
+++ main.cpp
@@ -3,11 +3,13 @@
 using namespace std;
 
 int main() {
-    vector<int> temp(100);
+    vector<int> temp(10);
+//
+//    for(int i = 0; i < temp.size(); i++) {
+//        temp[i] = i * i;
+//    }
 
-    for(int i = 0; i < temp.size(); i++) {
-        temp[i] = i * i;
-    }
+    temp = {0, 2, 4, 6, 8, 10, 12, 14, 16, 18};
 
     for(int i = 0; i < temp.size(); i++) {
         cout << i << ": " << temp[i] << endl;
