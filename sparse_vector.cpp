620537bda03cceff80ef8b8ac00f4c8651107780
Liam Lei <775138281@qq.com>
2017-09-16 20:28:36 -0400

    updates

--- sparse_vector.cpp
+++ sparse_vector.cpp
@@ -1,7 +1,9 @@
 //
-// Created by 雷昊 on 14/09/2017.
+// Created by Hao Lei on 14/09/2017.
 //
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class node {
@@ -61,16 +63,48 @@ void vector::add_node(int v, int pos) {
     node* temp = this->head;
     node* new_node = new node(v, pos);
 
-    while(temp->next != nullptr) {
+    // When there is no element in sparse vector.
+    if(this->head == nullptr) {
+        this->head = new_node;
+        this->size++;
+        this->num_nodes++;
+        return;
+    }
+
+    // When at least there is one element in the sparse vector.
+    while(temp != nullptr) {
+        // When position in current node is equal to pos,
+        // just refresh the value in current node.
         if(temp->position == pos) {
             temp->value = v;
+            break;
+        }
+        // When there is only one node in our sparse vector
+        // then we might not have a second node to compare its position
+        // so just add it to the list.
+        else if(this->size == 1 && temp->position > pos) {
+            new_node->next = temp;
+            temp = new_node;
+            this->size++;
+            this->num_nodes++;
+        }
+        // When we get a largest node position, just put it in the end.
+        else if(temp->position < pos && temp->next == nullptr) {
+            temp->next = new_node;
+            this->size++;
+            this->num_nodes++;
+            break;
+        }
+        // If the position of current node is less than pos but the one of the next is larger,
+        // then we find the right place to insert our new node.
+        else if(temp->position < pos && temp->next->position > pos) {
+            new_node->next = temp->next;
+            temp->next = new_node;
+            this->size++;
+            this->num_nodes++;
         }
 
-        if(temp->next->position > pos) {
-            node* temp2 = temp->next;
-            new_node->next = temp2;
-            temp->next = new_node;
-        }
+        temp = temp->next;
     }
 }
 
@@ -85,7 +119,13 @@ void vector::make_random_vector(int s, int n, int k) {
 }
 
 void vector::print() {
+    cout << endl;
+    node* temp = this->head;
 
+    while(temp != nullptr) {
+        cout << temp->value << " " << temp->position << "  ";
+        temp = temp->next;
+    }
 }
 
 vector vector::operator+(vector another) {
